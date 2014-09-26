549722b88adc811585f91937574cfa4672386b88
James Vickers <jamesvickers19@gmail.com>
2014-09-26 17:35:01 -0600

    Working on Arch 0.  Has some metric file writing setup, but needs to track the relevant data and actually write it.  Also needs behavior upated to actually move (still just sits there as in Arch 0)

--- Controller.c
+++ Controller.c
@@ -12,37 +12,66 @@
  *
  */
 
-#define ARCH_VERSION 0
+#define ARCH_VERSION 1
+#define MAX_TRIALS = 1000;
 
 int action = 0;
 int lifetime = 0;
+const int speed = 0.05;   // taken directly from the original version of this file.
+int trial = 1;
 
 void agents_controller( WORLD_TYPE *w )
 { 
+
+  simtime++;
 	
+  // First call of this life, do some setup 
   if (action++ == 0)
   { printf("*************************************\n");
     printf("ARCHITECTURE VERSION: %d\n",ARCH_VERSION);
     printf("*************************************\n");
+    
+    char time_metrics_filename[15], lifespan_metrics_filename[15];
+    snprintf(time_metrics_filename, sizeof(time_metrics_filename), "arch_%d_timestep.txt");
+    FILE* time_metrics_file = fopen(time_metrics_filename, 'w');
+    if (time_metrics_file == NULL)
+      printf("ERROR: Could not open %s for metric data writing!\n",time_metrics_filename);
+
+    snprintf(time_metrics_filename, sizeof(time_metrics_filename), "arch_%d_timestep.txt");
+    FILE* lifespan_metrics_file = fopen(lifespan_metrics_filename, "a+w");
+    if (lifespan_metrics_file == NULL)
+      printf("ERROR: Could not open %s for metric data writing!\n",lifespan_metrics_filename);
   }
 
   AGENT_TYPE *a ;
   
 	a = w->agents[0] ; /* get agent pointer */
 	
-	/* test if agent is alive. If so, increment simtime and do nothing (sit still...) */
+	/* test if agent is alive. If so, move in a straight line in the initial direction */
 	if( a->instate->metabolic_charge > 0.0 )
 	{  
-	    simtime++;
             lifetime++;
     	}
     	else
     	{
 	   printf("Agent died: simtime = %d, lifetime = %d\n",simtime, lifetime);
-           exit(0);
+           //TODO:  Reset lifetime, write data to file, remake agent in random orientation
+           
+           trial++;
+           if (trial > MAX_TRIALS)
+	   {  printf("%d trials completed, exiting\n", trial);
+	      fclose(time_metrics_file); 
+              fclose(lifespan_metrics_file);
+              exit(0);
+	   }
     	}
 
-        /* decrement metabolic charge by basil metabolism rate.  DO NOT REMOVE THIS CALL */
-       basal_metabolism_agent( a ) ;	
-  
-}
+	/* move the agents body */
+	set_forward_speed_agent( a, speed ) ;
+	move_body_agent( a ) ;
+
+	/* decrement metabolic charge by basil metabolism rate.  DO NOT REMOVE THIS CALL */
+	basal_metabolism_agent( a ) ;
+
+} 
+
